306d0d69d497c1bf12d1cdb0d0391f84478ba16b
Arturo Latorre <arturola@ymail.com>
2023-01-18 19:39:03 +0100

    Sprint_2 Se anyade la api-gateway

--- order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.programmingtechie.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,8 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
     //bean para generar un webClient
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();             //it will create the webClient instance and use Client Side Load Balancing to call the inventory-service (así sabrá a cual tiene que llamar
     }
 
 }
