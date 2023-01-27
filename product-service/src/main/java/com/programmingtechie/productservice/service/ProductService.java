7b2d154f2d2f3062b9594c633feccce4486b89f4
Arturo Latorre <arturola@ymail.com>
2023-01-27 14:24:09 +0100

    Sprint_5 Se anyade dependency de postgres

--- product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -6,6 +6,7 @@ import com.programmingtechie.productservice.model.Product;
 import com.programmingtechie.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class ProductService {
-
+    @Autowired
     private final ProductRepository productRepository;
 
     public void createProduct(ProductRequest productRequest) {
